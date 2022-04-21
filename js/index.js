5fbdd61995d7eb2530c664ea4ca581513d3a8573
liamth97 <87107360+liamth97@users.noreply.github.com>
2022-04-22 00:20:30 +1000

    created new layout
    cards now stack, added some more styling to the cards

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function getBoredAPI() {
 	// Clear Cards
 	suggestions.innerHTML = '';
 
-	for (let index = 0; index < 3; index++) {
+	for (let index = 0; index < 100; index++) {
 		const response = await fetch('http://www.boredapi.com/api/activity/');
 		const data = await response.json();
 		console.log(data);
@@ -20,32 +20,35 @@ function createSuggestionCard(data) {
 	// Get suggestions ID
 	const suggestions = document.getElementById('suggestions');
 
-	// Create cards with new suggestions
+	//* Create cards with new suggestions
 	//! Bootstrap classes are used
 
 	// Creates div with "col" class
 	const col = document.createElement('div');
-	col.classList.add('col-lg-4');
+	col.classList.add('col-12');
+	col.classList.add('col-xxl-6');
+	col.setAttribute('data-aos', 'fade-right');
+	col.setAttribute('data-aos', 'fade-right');
 	suggestions.appendChild(col);
 
 	// Creates div with "card" class
 	const card = document.createElement('div');
-	card.classList.add('card');
+	card.classList.add('card', 'border-dark', 'shadow');
 	col.appendChild(card);
 
-	// Create card body
+	//* Create card body
 	const cardBody = document.createElement('div');
 	cardBody.classList.add('card-body');
 	card.appendChild(cardBody);
 
-	// Create card title
+	//* Create card title
 	const cardTitle = document.createElement('h5');
 	const cardTitleText = document.createTextNode(data.activity);
 	cardTitle.classList.add('card-title');
 	cardTitle.appendChild(cardTitleText);
 	cardBody.appendChild(cardTitle);
 
-	// Create card text
+	//* Create card text
 	// type
 	let cardText = document.createElement('p');
 	let cardTextText = document.createTextNode('This activity is ' + data.type);
