7cbe52babf4fcbb3b7792acd054572bbfb500aeb
liamth97 <87107360+liamth97@users.noreply.github.com>
2022-04-21 16:11:44 +1000

    get api info

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+// Get btn by ID
+const boredBtn = document.getElementById('boredBtn');
+boredBtn.addEventListener('click', getBoredAPI);
 
+// Get's API response and converts it to JSON
+async function getBoredAPI() {
+	const response = await fetch('http://www.boredapi.com/api/activity/');
+	const data = await response.json();
+	console.log(data.activity);
+	console.log(data.type);
+}
+
+// function printBored() {}
