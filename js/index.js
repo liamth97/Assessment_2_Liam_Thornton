945547af1fdd1f76229539f53b5a5b23fa1fe6b8
liamth97 <87107360+liamth97@users.noreply.github.com>
2022-04-21 17:21:00 +1000

    updates some js
    there is now some more information displayed in the cards

--- js/index.js
+++ js/index.js
@@ -4,10 +4,15 @@ boredBtn.addEventListener('click', getBoredAPI);
 
 // Get's API response and converts it to JSON
 async function getBoredAPI() {
-	const response = await fetch('http://www.boredapi.com/api/activity/');
-	const data = await response.json();
-	console.log(data);
-	createSuggestionCard(data);
+	// Clear Cards
+	suggestions.innerHTML = '';
+
+	for (let index = 0; index < 3; index++) {
+		const response = await fetch('http://www.boredapi.com/api/activity/');
+		const data = await response.json();
+		console.log(data);
+		createSuggestionCard(data);
+	}
 }
 
 // Function to create a suggestion card
@@ -15,42 +20,50 @@ function createSuggestionCard(data) {
 	// Get suggestions ID
 	const suggestions = document.getElementById('suggestions');
 
-	// Clear Cards
-	suggestions.innerHTML = '';
+	// Create cards with new suggestions
+	//! Bootstrap classes are used
 
-	// Loop to create three cards
+	// Creates div with "col" class
+	const col = document.createElement('div');
+	col.classList.add('col-lg-4');
+	suggestions.appendChild(col);
 
-	for (let index = 0; index < 3; index++) {
-		// Create cards with new suggestions
-		//! Bootstrap classes are used
+	// Creates div with "card" class
+	const card = document.createElement('div');
+	card.classList.add('card');
+	col.appendChild(card);
 
-		// Creates div with "col" class
-		const col = document.createElement('div');
-		col.classList.add('col-lg-4');
-		suggestions.appendChild(col);
+	// Create card body
+	const cardBody = document.createElement('div');
+	cardBody.classList.add('card-body');
+	card.appendChild(cardBody);
 
-		// Creates div with "card" class
-		const card = document.createElement('div');
-		card.classList.add('card');
-		col.appendChild(card);
+	// Create card title
+	const cardTitle = document.createElement('h5');
+	const cardTitleText = document.createTextNode(data.activity);
+	cardTitle.classList.add('card-title');
+	cardTitle.appendChild(cardTitleText);
+	cardBody.appendChild(cardTitle);
 
-		// Create card body
-		const cardBody = document.createElement('div');
-		cardBody.classList.add('card-body');
-		card.appendChild(cardBody);
-
-		// Create card title
-		const cardTitle = document.createElement('h5');
-		const cardTitleText = document.createTextNode('Title');
-		cardTitle.classList.add('card-title');
-		cardTitle.appendChild(cardTitleText);
-		cardBody.appendChild(cardTitle);
-
-		// Create card text
-		const cardText = document.createElement('p');
-		const cardTextText = document.createTextNode('Text');
-		cardText.classList.add('card-text');
-		cardText.appendChild(cardTextText);
-		cardBody.appendChild(cardText);
-	}
+	// Create card text
+	// type
+	let cardText = document.createElement('p');
+	let cardTextText = document.createTextNode('This activity is ' + data.type);
+	cardText.classList.add('card-text');
+	cardText.appendChild(cardTextText);
+	cardBody.appendChild(cardText);
+	// participants
+	cardText = document.createElement('p');
+	cardTextText = document.createTextNode(
+		'This activity requires ' + data.participants + ' participants'
+	);
+	cardText.classList.add('card-text');
+	cardText.appendChild(cardTextText);
+	cardBody.appendChild(cardText);
+	// price
+	cardText = document.createElement('p');
+	cardTextText = document.createTextNode('Price: ' + data.price);
+	cardText.classList.add('card-text');
+	cardText.appendChild(cardTextText);
+	cardBody.appendChild(cardText);
 }
