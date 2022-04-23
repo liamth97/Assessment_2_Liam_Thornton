50a88bdf5f83f86790d16f90f3b7d75206138fcb
liamth97 <87107360+liamth97@users.noreply.github.com>
2022-04-23 14:28:03 +1000

    updated styling and some functionality
    the cards now auto load. made it so as your scroll new cards will be auto generated by running the function using scroll event listener

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-// Get btn by ID
+//* Get btn by ID
 const boredBtn = document.getElementById('boredBtn');
-boredBtn.addEventListener('click', getBoredAPI);
 
-// Get's API response and converts it to JSON
+//* Runs getBoredAPI function on load
+window.addEventListener('load', getBoredAPI);
+
+//* Runs getBoredAPI function on scroll
+//* This continually generates content for the user
+window.addEventListener('scroll', getBoredAPI);
+
+//* Get's API response and converts it to JSON
 async function getBoredAPI() {
-	// Clear Cards
-	suggestions.innerHTML = '';
-
-	for (let index = 0; index < 100; index++) {
+	//* For loop that gets new request to generate new items
+	for (let index = 0; index < 24; index++) {
 		const response = await fetch('http://www.boredapi.com/api/activity/');
 		const data = await response.json();
 		console.log(data);
@@ -15,7 +19,7 @@ async function getBoredAPI() {
 	}
 }
 
-// Function to create a suggestion card
+//* Function to create a suggestion card
 function createSuggestionCard(data) {
 	// Get suggestions ID
 	const suggestions = document.getElementById('suggestions');
@@ -23,17 +27,17 @@ function createSuggestionCard(data) {
 	//* Create cards with new suggestions
 	//! Bootstrap classes are used
 
-	// Creates div with "col" class
+	//* Creates div with "col" class
 	const col = document.createElement('div');
-	col.classList.add('col-12');
-	col.classList.add('col-xxl-6');
+	col.classList.add('col-sm-12', 'col-xl-3', 'col-lg-4', 'col-md-6');
 	col.setAttribute('data-aos', 'fade-right');
 	col.setAttribute('data-aos', 'fade-right');
 	suggestions.appendChild(col);
 
-	// Creates div with "card" class
+	//* Creates div with "card" class
 	const card = document.createElement('div');
-	card.classList.add('card', 'border-dark', 'shadow');
+	card.classList.add('card', 'border-dark', 'bg-dark', 'shadow', 'text-light');
+	card.setAttribute('id', 'suggestionCard');
 	col.appendChild(card);
 
 	//* Create card body
